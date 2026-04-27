d657127d65bfb1561eb8108f49c248f0c08012e6
danendra <pr.endra48@gmail.com>
2026-04-27 09:59:08 +0700

    fix: toaster at app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
+        <Toaster />
       <Sonner />
       <BrowserRouter>
         <ScrollToTop />
